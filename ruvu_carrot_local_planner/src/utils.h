12ca6cd48ff68371b7486c55309e034a33f03c41
Ramon Wijnands <ramon@ruvu.nl>
2019-05-03 14:17:40 +0200

    Cleanup unused functions

--- ruvu_carrot_local_planner/src/utils.h
+++ ruvu_carrot_local_planner/src/utils.h
@@ -33,22 +33,4 @@ int sgn(T val)
 {
   return (T(0) < val) - (val < T(0));
 }
-
-template <typename T>
-inline constexpr int signum(T x, std::false_type is_signed)
-{
-  return T(0) < x;
-}
-
-template <typename T>
-inline constexpr int signum(T x, std::true_type is_signed)
-{
-  return (T(0) < x) - (x < T(0));
-}
-
-template <typename T>
-inline constexpr int signum(T x)
-{
-  return signum(x, std::is_signed<T>());
-}
 }
