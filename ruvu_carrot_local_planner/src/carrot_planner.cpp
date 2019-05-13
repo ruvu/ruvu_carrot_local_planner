e08168c41d98f1c4b91610674f7326e8880d36c4
Ramon Wijnands <ramon@ruvu.nl>
2019-05-13 17:23:49 +0200

    Refactor the carrot planner into two files
    It now uses the same structure as the DWA planner.

---
+++ ruvu_carrot_local_planner/src/carrot_planner.cpp
@@ -0,0 +1,185 @@
+#include "./carrot_planner.h"
+
+#include <base_local_planner/goal_functions.h>
+#include <visualization_msgs/MarkerArray.h>
+
+#include "./utils.h"
+
+namespace ruvu_carrot_local_planner
+{
+CarrotPlanner::CarrotPlanner(ros::NodeHandle private_nh, base_local_planner::LocalPlannerUtil* planner_util)
+  : planner_util_(planner_util)
+  , sim_period_(getSimPeriodParam(private_nh))
+  , debug_pub_(private_nh.advertise<visualization_msgs::MarkerArray>("visualization", 1))
+{
+}
+
+void CarrotPlanner::setPlan(const std::vector<geometry_msgs::PoseStamped>& orig_global_plan)
+{
+  state_ = State::DRIVING;
+}
+
+CarrotPlanner::Outcome CarrotPlanner::computeVelocityCommands(const std::vector<geometry_msgs::PoseStamped>& path,
+                                                              const tf::Stamped<tf::Pose>& global_pose,
+                                                              const geometry_msgs::Twist& global_vel,
+                                                              geometry_msgs::Twist& cmd_vel, std::string& message)
+{
+  // Look for the closest point on the path
+  auto closest = min_by(path.begin(), path.end(), [&global_pose](const geometry_msgs::PoseStamped& ps) {
+    return base_local_planner::getGoalPositionDistance(global_pose, ps.pose.position.x, ps.pose.position.y);
+  });
+
+  tf::Point goal;
+  tf::pointMsgToTF(path.back().pose.position, goal);
+  auto goal_error = goal - global_pose.getOrigin();
+  double goal_distance = goal_error.length();
+
+  if (goal_distance < parameters_.carrot_distance && state_ != State::ARRIVING)
+  {
+    ROS_INFO_NAMED("ruvu_carrot_local_planner", "I'm close to the goal, let's go to the arriving state");
+    state_ = State::ARRIVING;
+    arriving_angle_ = atan2(goal_error.getY(), goal_error.getX());
+  }
+
+  tf::Stamped<tf::Pose> carrot(tf::Pose::getIdentity(), global_pose.stamp_, global_pose.frame_id_);
+  switch (state_)
+  {
+    case State::DRIVING:
+      message = "Driving";
+      computeCarrot(path, closest, carrot);
+      break;
+    case State::ARRIVING:
+    {
+      message = "Arriving";
+      // The carrot walks along the arriving angle from the goal
+      auto direction = tf::quatRotate(tf::createQuaternionFromYaw(arriving_angle_),
+                                      tf::Vector3(parameters_.carrot_distance - goal_distance, 0, 0));
+      if (direction.dot(goal_error) < 0)
+      {
+        message = "Goal overshoot detected";
+        ROS_WARN_STREAM_THROTTLE_NAMED(5, "ruvu_carrot_local_planner", message);
+        return Outcome::MISSED_GOAL;
+      }
+      carrot.setOrigin(goal + direction);
+      break;
+    }
+    default:
+      assert(false);
+  }
+
+  publishDebugCarrot(carrot);
+
+  auto limits = planner_util_->getCurrentLimits();
+
+  {
+    double a = limits.acc_lim_x;
+    double m = parameters_.slow_down_margin;
+
+    double v1 = -a * m - sqrt(a * (a * m * m + 2 * goal_distance));
+    double v2 = -a * m + sqrt(a * (a * m * m + 2 * goal_distance));
+    ROS_DEBUG_NAMED("ruvu_carrot_local_planner", "v1=%f, v2=%f", v1, v2);
+
+    double v_max = v2;
+    ROS_DEBUG_STREAM_NAMED("ruvu_carrot_local_planner", "v_max: " << v_max);
+    cmd_vel.linear.x = v_max > limits.max_vel_x ? limits.max_vel_x : v_max;
+    cmd_vel.linear.x = cmd_vel.linear.x < limits.min_trans_vel ? limits.min_trans_vel : cmd_vel.linear.x;
+  }
+
+  auto error = carrot.getOrigin() - global_pose.getOrigin();
+  double angle_to_carrot = atan2(error.getY(), error.getX());
+  double carrot_error = base_local_planner::getGoalOrientationAngleDifference(global_pose, angle_to_carrot);
+
+  // determine the position of the carrot relative to the closest point
+  tf::Point closest_position;
+  tf::pointMsgToTF(closest->pose.position, closest_position);
+  auto rel_carrot = carrot.getOrigin() - closest_position;
+
+  // Determine the angle of the poses on the path relative to the direction of the path. The direciton of the path can
+  // be estimated by looking at the location of the carrot relative the the closest point. That is "forward" along the
+  // path.
+  double path_direction = angles::shortest_angular_distance(tf::getYaw(closest->pose.orientation),
+                                                            atan2(rel_carrot.getY(), rel_carrot.getX()));
+  bool switch_direction = !(-M_PI_2 < path_direction && path_direction < M_PI_2);
+
+  if (switch_direction)
+  {
+    carrot_error = angles::normalize_angle(carrot_error + M_PI);
+  }
+
+  cmd_vel.angular.z = parameters_.p_angle * carrot_error;
+
+  if (switch_direction)
+  {
+    cmd_vel.linear.x = -cmd_vel.linear.x;
+  }
+
+  // If we rotate faster than possible, scale back the both velocities
+  if (fabs(cmd_vel.angular.z) > limits.max_rot_vel)
+  {
+    cmd_vel.linear.x = cmd_vel.linear.x * limits.max_rot_vel / fabs(cmd_vel.angular.z);
+    cmd_vel.angular.z = cmd_vel.angular.z * limits.max_rot_vel / fabs(cmd_vel.angular.z);
+  }
+
+  // Smooth the required velocity with the maximum acceleration
+  double max_x_step = limits.acc_lim_x * sim_period_;
+  double max_theta_step = limits.acc_lim_theta * sim_period_;
+  if (fabs(cmd_vel.linear.x - global_vel.linear.x) > max_x_step)
+  {
+    cmd_vel.linear.x = global_vel.linear.x + sgn(cmd_vel.linear.x - global_vel.linear.x) * max_x_step;
+  }
+  if (fabs(cmd_vel.angular.z - global_vel.angular.z) > max_theta_step)
+  {
+    cmd_vel.angular.z = global_vel.angular.z + sgn(cmd_vel.angular.z - global_vel.angular.z) * max_theta_step;
+  }
+
+  return Outcome::OK;
+}
+
+void CarrotPlanner::computeCarrot(const std::vector<geometry_msgs::PoseStamped>& path,
+                                  std::vector<geometry_msgs::PoseStamped>::const_iterator it,
+                                  tf::Stamped<tf::Pose>& carrot)
+{
+  // Walk along the path forward and count the distance. When carrot_distance has been walked, the carrot is found.
+  double distance = parameters_.carrot_distance;
+
+  tf::Stamped<tf::Pose> previous;
+  tf::Stamped<tf::Pose> current;
+  tf::poseStampedMsgToTF(*it, current);
+  for (; ++it < path.end();)
+  {
+    // Update previous & current
+    previous = current;
+    tf::poseStampedMsgToTF(*it, current);
+
+    distance -= (current.getOrigin() - previous.getOrigin()).length();
+    if (distance <= 0)
+    {
+      break;
+    }
+  }
+
+  carrot = current;
+}
+
+void CarrotPlanner::publishDebugCarrot(const tf::Stamped<tf::Pose>& carrot)
+{
+  visualization_msgs::Marker marker;
+  marker.header.frame_id = carrot.frame_id_;
+  marker.header.stamp = carrot.stamp_;
+  marker.ns = "carrot";
+  marker.id = 0;
+  marker.type = visualization_msgs::Marker::SPHERE;
+  marker.action = visualization_msgs::Marker::ADD;
+  tf::poseTFToMsg(carrot, marker.pose);
+  marker.scale.x = 0.2;
+  marker.scale.y = 0.2;
+  marker.scale.z = 0.2;
+  marker.color.a = 1.0;
+  marker.color.r = 1.0;
+  marker.color.g = 0.5;
+  marker.color.b = 0.0;
+  visualization_msgs::MarkerArray markers;
+  markers.markers = { marker };
+  debug_pub_.publish(markers);
+}
+}
--- ruvu_carrot_local_planner/src/carrot_planner_ros.cpp
+++ ruvu_carrot_local_planner/src/carrot_planner_ros.cpp
@@ -6,7 +6,6 @@
 
 #include <base_local_planner/goal_functions.h>
 #include <nav_msgs/Path.h>
-#include <visualization_msgs/MarkerArray.h>
 
 #include "./utils.h"
 
@@ -50,13 +49,10 @@ void CarrotPlannerROS::reconfigureCB(CarrotPlannerConfig& config, uint32_t level
   planner_util_.reconfigureCB(limits, config.restore_defaults);
 
   // initialize parameters specific to this module
+  CarrotPlanner::Parameters parameters;
   parameters.carrot_distance = config.carrot_distance;
   parameters.p_angle = config.p_angle;
   parameters.slow_down_margin = config.slow_down_margin;
-}
-
-CarrotPlannerROS::CarrotPlannerROS() : odom_helper_("odom")
-{
 }
 
 void CarrotPlannerROS::initialize(std::string name, tf::TransformListener* tf, costmap_2d::Costmap2DROS* costmap_ros)
@@ -66,7 +62,6 @@ void CarrotPlannerROS::initialize(std::string name, tf::TransformListener* tf, c
     ros::NodeHandle private_nh("~/" + name);
     g_plan_pub_ = private_nh.advertise<nav_msgs::Path>("global_plan", 1);
     l_plan_pub_ = private_nh.advertise<nav_msgs::Path>("local_plan", 1);
-    debug_pub_ = private_nh.advertise<visualization_msgs::MarkerArray>("visualization", 1);
     tf_ = tf;
     costmap_ros_ = costmap_ros;
 
@@ -75,16 +70,17 @@ void CarrotPlannerROS::initialize(std::string name, tf::TransformListener* tf, c
 
     planner_util_.initialize(tf, costmap, costmap_ros_->getGlobalFrameID());
 
+    // create the actual planner that we'll use.. it'll configure itself from the parameter server
+    carrot_planner_.reset(new CarrotPlanner(private_nh, &planner_util_));
+
     if (private_nh.getParam("odom_topic", odom_topic_))
     {
       odom_helper_.setOdomTopic(odom_topic_);
     }
 
-    sim_period_ = getSimPeriodParam(private_nh);
-
     initialized_ = true;
 
-    dsrv_ = new dynamic_reconfigure::Server<CarrotPlannerConfig>(private_nh);
+    dsrv_.reset(new dynamic_reconfigure::Server<CarrotPlannerConfig>(private_nh));
     dynamic_reconfigure::Server<CarrotPlannerConfig>::CallbackType cb =
         boost::bind(&CarrotPlannerROS::reconfigureCB, this, _1, _2);
     dsrv_->setCallback(cb);
@@ -106,9 +102,8 @@ bool CarrotPlannerROS::setPlan(const std::vector<geometry_msgs::PoseStamped>& or
   // when we get a new plan, we also want to clear any latch we may have on goal tolerances
   latchedStopRotateController_.resetLatching();
 
-  state_ = State::DRIVING;
-
   ROS_INFO_NAMED("ruvu_carrot_local_planner", "Got new plan");
+  carrot_planner_->setPlan(orig_global_plan);
   return planner_util_.setPlan(orig_global_plan);
 }
 
@@ -153,178 +148,6 @@ void CarrotPlannerROS::publishGlobalPlan(std::vector<geometry_msgs::PoseStamped>
   base_local_planner::publishPlan(path, g_plan_pub_);
 }
 
-void CarrotPlannerROS::publishDebugCarrot(const tf::Stamped<tf::Pose>& carrot)
-{
-  visualization_msgs::Marker marker;
-  marker.header.frame_id = carrot.frame_id_;
-  marker.header.stamp = carrot.stamp_;
-  marker.ns = "carrot";
-  marker.id = 0;
-  marker.type = visualization_msgs::Marker::SPHERE;
-  marker.action = visualization_msgs::Marker::ADD;
-  tf::poseTFToMsg(carrot, marker.pose);
-  marker.scale.x = 0.2;
-  marker.scale.y = 0.2;
-  marker.scale.z = 0.2;
-  marker.color.a = 1.0;
-  marker.color.r = 1.0;
-  marker.color.g = 0.5;
-  marker.color.b = 0.0;
-  visualization_msgs::MarkerArray markers;
-  markers.markers = { marker };
-  debug_pub_.publish(markers);
-}
-
-CarrotPlannerROS::~CarrotPlannerROS()
-{
-  // make sure to clean things up
-  delete dsrv_;
-}
-
-CarrotPlannerROS::Outcome CarrotPlannerROS::carrotComputeVelocityCommands(
-    const std::vector<geometry_msgs::PoseStamped>& path, const tf::Stamped<tf::Pose>& global_pose,
-    geometry_msgs::Twist& cmd_vel, std::string& message)
-{
-  // Look for the closest point on the path
-  auto closest = min_by(path.begin(), path.end(), [&global_pose](const geometry_msgs::PoseStamped& ps) {
-    return base_local_planner::getGoalPositionDistance(global_pose, ps.pose.position.x, ps.pose.position.y);
-  });
-
-  tf::Point goal;
-  tf::pointMsgToTF(path.back().pose.position, goal);
-  auto goal_error = goal - global_pose.getOrigin();
-  double goal_distance = goal_error.length();
-
-  if (goal_distance < parameters.carrot_distance && state_ != State::ARRIVING)
-  {
-    ROS_INFO_NAMED("ruvu_carrot_local_planner", "I'm close to the goal, let's go to the arriving state");
-    state_ = State::ARRIVING;
-    arriving_angle_ = atan2(goal_error.getY(), goal_error.getX());
-  }
-
-  tf::Stamped<tf::Pose> carrot(tf::Pose::getIdentity(), global_pose.stamp_, global_pose.frame_id_);
-  switch (state_)
-  {
-    case State::DRIVING:
-      message = "Driving";
-      computeCarrot(path, closest, carrot);
-      break;
-    case State::ARRIVING:
-    {
-      message = "Arriving";
-      // The carrot walks along the arriving angle from the goal
-      auto direction = tf::quatRotate(tf::createQuaternionFromYaw(arriving_angle_),
-                                      tf::Vector3(parameters.carrot_distance - goal_distance, 0, 0));
-      if (direction.dot(goal_error) < 0)
-      {
-        message = "Goal overshoot detected";
-        ROS_WARN_STREAM_THROTTLE_NAMED(5, "ruvu_carrot_local_planner", message);
-        return Outcome::MISSED_GOAL;
-      }
-      carrot.setOrigin(goal + direction);
-      break;
-    }
-    default:
-      assert(false);
-  }
-
-  publishDebugCarrot(carrot);
-
-  nav_msgs::Odometry odom;
-  odom_helper_.getOdom(odom);
-  auto limits = planner_util_.getCurrentLimits();
-
-  {
-    double a = limits.acc_lim_x;
-    double m = parameters.slow_down_margin;
-
-    double v1 = -a * m - sqrt(a * (a * m * m + 2 * goal_distance));
-    double v2 = -a * m + sqrt(a * (a * m * m + 2 * goal_distance));
-    ROS_DEBUG_NAMED("ruvu_carrot_local_planner", "v1=%f, v2=%f", v1, v2);
-
-    double v_max = v2;
-    ROS_DEBUG_STREAM_NAMED("ruvu_carrot_local_planner", "v_max: " << v_max);
-    cmd_vel.linear.x = v_max > limits.max_vel_x ? limits.max_vel_x : v_max;
-    cmd_vel.linear.x = cmd_vel.linear.x < limits.min_trans_vel ? limits.min_trans_vel : cmd_vel.linear.x;
-  }
-
-  auto error = carrot.getOrigin() - global_pose.getOrigin();
-  double angle_to_carrot = atan2(error.getY(), error.getX());
-  double carrot_error = base_local_planner::getGoalOrientationAngleDifference(global_pose, angle_to_carrot);
-
-  // determine the position of the carrot relative to the closest point
-  tf::Point closest_position;
-  tf::pointMsgToTF(closest->pose.position, closest_position);
-  auto rel_carrot = carrot.getOrigin() - closest_position;
-
-  // Determine the angle of the poses on the path relative to the direction of the path. The direciton of the path can
-  // be estimated by looking at the location of the carrot relative the the closest point. That is "forward" along the
-  // path.
-  double path_direction = angles::shortest_angular_distance(tf::getYaw(closest->pose.orientation),
-                                                            atan2(rel_carrot.getY(), rel_carrot.getX()));
-  bool switch_direction = !(-M_PI_2 < path_direction && path_direction < M_PI_2);
-
-  if (switch_direction)
-  {
-    carrot_error = angles::normalize_angle(carrot_error + M_PI);
-  }
-
-  cmd_vel.angular.z = parameters.p_angle * carrot_error;
-
-  if (switch_direction)
-  {
-    cmd_vel.linear.x = -cmd_vel.linear.x;
-  }
-
-  // If we rotate faster than possible, scale back the both velocities
-  if (fabs(cmd_vel.angular.z) > limits.max_rot_vel)
-  {
-    cmd_vel.linear.x = cmd_vel.linear.x * limits.max_rot_vel / fabs(cmd_vel.angular.z);
-    cmd_vel.angular.z = cmd_vel.angular.z * limits.max_rot_vel / fabs(cmd_vel.angular.z);
-  }
-
-  // Smooth the required velocity with the maximum acceleration
-  double max_x_step = limits.acc_lim_x * sim_period_;
-  double max_theta_step = limits.acc_lim_theta * sim_period_;
-  if (fabs(cmd_vel.linear.x - odom.twist.twist.linear.x) > max_x_step)
-  {
-    cmd_vel.linear.x = odom.twist.twist.linear.x + sgn(cmd_vel.linear.x - odom.twist.twist.linear.x) * max_x_step;
-  }
-  if (fabs(cmd_vel.angular.z - odom.twist.twist.angular.z) > max_theta_step)
-  {
-    cmd_vel.angular.z =
-        odom.twist.twist.angular.z + sgn(cmd_vel.angular.z - odom.twist.twist.angular.z) * max_theta_step;
-  }
-
-  return Outcome::OK;
-}
-
-void CarrotPlannerROS::computeCarrot(const std::vector<geometry_msgs::PoseStamped>& path,
-                                     std::vector<geometry_msgs::PoseStamped>::const_iterator it,
-                                     tf::Stamped<tf::Pose>& carrot)
-{
-  // Walk along the path forward and count the distance. When carrot_distance has been walked, the carrot is found.
-  double distance = parameters.carrot_distance;
-
-  tf::Stamped<tf::Pose> previous;
-  tf::Stamped<tf::Pose> current;
-  tf::poseStampedMsgToTF(*it, current);
-  for (; ++it < path.end();)
-  {
-    // Update previous & current
-    previous = current;
-    tf::poseStampedMsgToTF(*it, current);
-
-    distance -= (current.getOrigin() - previous.getOrigin()).length();
-    if (distance <= 0)
-    {
-      break;
-    }
-  }
-
-  carrot = current;
-}
-
 uint32_t CarrotPlannerROS::computeVelocityCommands(const geometry_msgs::PoseStamped& robot_pose,
                                                    const geometry_msgs::TwistStamped& robot_velocity,
                                                    geometry_msgs::TwistStamped& cmd_vel, std::string& message)
@@ -360,8 +183,8 @@ uint32_t CarrotPlannerROS::computeVelocityCommands(const geometry_msgs::PoseStam
     publishLocalPlan(local_plan);
     auto limits = planner_util_.getCurrentLimits();
     if (latchedStopRotateController_.computeVelocityCommandsStopRotate(
-            cmd_vel.twist, limits.getAccLimits(), sim_period_, &planner_util_, odom_helper_, current_pose_,
-            boost::bind(&CarrotPlannerROS::checkTrajectory, this, _1, _2, _3)))
+            cmd_vel.twist, limits.getAccLimits(), carrot_planner_->getSimPeriod(), &planner_util_, odom_helper_,
+            current_pose_, boost::bind(&CarrotPlannerROS::checkTrajectory, this, _1, _2, _3)))
     {
       return 0;
     }
@@ -372,10 +195,13 @@ uint32_t CarrotPlannerROS::computeVelocityCommands(const geometry_msgs::PoseStam
   }
   else
   {
-    auto outcome = carrotComputeVelocityCommands(transformed_plan, current_pose_, cmd_vel.twist, message);
+    nav_msgs::Odometry odom;
+    odom_helper_.getOdom(odom);
+    auto outcome = carrot_planner_->computeVelocityCommands(transformed_plan, current_pose_, odom.twist.twist,
+                                                            cmd_vel.twist, message);
     switch (outcome)
     {
-      case Outcome::OK:
+      case CarrotPlanner::Outcome::OK:
         ROS_DEBUG_NAMED("ruvu_carrot_local_planner", "Computed the following cmd_vel: %.3lf, %.3lf, %.3lf",
                         cmd_vel.twist.linear.x, cmd_vel.twist.linear.y, cmd_vel.twist.angular.z);
         publishGlobalPlan(transformed_plan);
