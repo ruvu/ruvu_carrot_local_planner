e781acd072662b20baa926a3e4fdaee461fa37b9
Ramon Wijnands <ramon@ruvu.nl>
2020-07-31 10:57:14 +0200

    Remove the need for a roscore during testing

--- ruvu_carrot_local_planner/test/test_utils.cpp
+++ ruvu_carrot_local_planner/test/test_utils.cpp
@@ -20,7 +20,5 @@ TEST(TestSgn, 1)
 // Run all the tests that were declared with TEST()
 int main(int argc, char **argv){
   testing::InitGoogleTest(&argc, argv);
-  ros::init(argc, argv, "tester");
-  ros::NodeHandle nh;
   return RUN_ALL_TESTS();
 }
